b15b1feed628661d292e801912a4948c76eccb53
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-06-29 03:24:59 -0700

    shekoofeh-shojai

--- backend/routes/workRouts.js
+++ backend/routes/workRouts.js
@@ -2,11 +2,31 @@ const express=require("express");
 //export router from express
 const router=express.Router();
 
-//get routers name /
-router.get('/',()=>{
-
+//get all woroutes
+//woroutes is routes
+router.get('/',(req,res)=>{
+res.json({mssg:'GET all workouts'})
 
 })
 
+//GET SINGLE WORKOUT
+router.get('/:id',(req,res)=>{
+    res.json({mssg:'GET SINGLE WORKOUTES'})
+})
 
+//POST A NEW WORKOUT
+router.post('/',(req,res)=>{
+    res.json({mess:'POST A NEW WORKOUT'})
+})
+
+
+//DELETE A NEW WORKOUT
+router.delete('/:id',(req,res)=>{
+    res.json({mess:'DELETE A NEW WORKOUT !!!'})
+})
+
+// UPDATE A NEW WORKOUT
+router.patch("/:id", (req, res) => {
+  res.json({ mess: "UPDUTE A NEW WORKOUT 3>" });
+});
 module.exports=router
