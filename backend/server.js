a8ee1acdd6b3b5f7bcbed5f0fcb9d643ca754945
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-06-29 03:26:31 -0700

    shekoofeh

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,31 @@
 const express=require("express");
 require("dotenv").config()
+//REQUIRE ROUTERS
+const workoutRoutes=require('./routes/workRouts')
+
 const app=express();
 
 //middelwere
+//access to midelwere for update data
+// middelwere is requist hundeler
+app.use(express.json())
+
 app.use((req,res,next)=>{
 console.log(req.path,req.method);
 next()
 
 })
-app.get("/",(req,res)=>{
-    res.json({mssg:"welcome to app"})
+//ROUTES
+// app.get("/",(req,res)=>{
+//     res.json({mssg:"welcome to app"})
 
-})
+// })
+
+//go to the workroutes file 
+//=>    /  in end api/woroutes
+//api/woroutes/
+app.use('/api/woroutes',workoutRoutes)
+
 //listen to request
 app.listen(process.env.PORT,()=>{
    console.log("listening on port", process.env.PORT);
