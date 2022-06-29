8e8622a45fc2758128b542fa06512f04572d5247
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-06-29 02:43:44 -0700

    shekoofeh-shojai

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ npm init -y
 npm install express
 //install globally
 install -g nodemon
+npm run dev
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express=require("express");
-
+require("dotenv").config()
 const app=express();
 
+//middelwere
+app.use((req,res,next)=>{
+console.log(req.path,req.method);
+next()
+
+})
+app.get("/",(req,res)=>{
+    res.json({mssg:"welcome to app"})
+
+})
 //listen to request
-app.listen(4100,()=>{
-    console.log("listening on port 4000");
+app.listen(process.env.PORT,()=>{
+   console.log("listening on port", process.env.PORT);
 })
