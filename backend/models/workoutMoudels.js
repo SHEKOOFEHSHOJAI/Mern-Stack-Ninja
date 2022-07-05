6e66cfee2c80d385f7e41d757e1f62c2314b790f
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-07-05 03:27:31 -0700

    shekoofeh

--- backend/models/workoutMoudels.js
+++ backend/models/workoutMoudels.js
@@ -1,6 +1,6 @@
-const mongoose=require("mongoose");
+const mongoose = require('mongoose');
 
-const Schema=mongoose.Schema
+const Schema = mongoose.Schema;
 
 const workoutSchema=new Schema({
     title:{
@@ -20,4 +20,4 @@ const workoutSchema=new Schema({
 },{timestamps:true})
 
 
-module.exports=mongoose.model('workout',workoutSchema)
+module.exports = mongoose.model("workout", workoutSchema);
